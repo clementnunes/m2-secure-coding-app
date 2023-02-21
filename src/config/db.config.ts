35c17f2d3f42ed36a8006dfdd0fe7d9583c5c5d2
Clement <clement.nunes@efrei.net>
2023-02-21 10:54:35 +0100

    Fixed

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,6 +2,8 @@ import {EnvConfig} from "./env.config";
 
 export class DBConfig {
     static HOST : string = EnvConfig.load("DB_HOST") as string;
+    static HOST_PROD : string = EnvConfig.load("DB_HOST_PROD") as string;
+    static APP_PROD : string = EnvConfig.load("APP_PROD") as string;
     static USERNAME : string = EnvConfig.load("DB_USERNAME") as string;
 
     static PASSWORD : string = EnvConfig.load("DB_PASSWORD") as string;
